eea886461e1ee223912f24b094580250f7358f3b
Tolga Altinordu <tolgaaltinordu@gmail.com>
2024-03-21 16:25:01 +0300

    clerk has installed and configured

---
+++ app/sign-in/[[...sign-in]]/page.js
@@ -0,0 +1,11 @@
+import { SignIn } from '@clerk/nextjs'
+
+function SignInPage() {
+    return (
+        <div className='min-h-screen flex justify-center items-center'>
+            <SignIn />
+        </div>
+    )
+}
+
+export default SignInPage
